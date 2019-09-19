693ea20298b64b53abcbd30d66ae34c4a109c465
elizabethkirschner <eekirschner@wpi.edu>
2019-09-19 01:05:32 -0400

    updates
    Signed-off-by: elizabethkirschner <eekirschner@wpi.edu>

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,13 +1,16 @@
 
 var table // table needs to be global
 var row // need the row to set up edit
-var editButton
 var formDisplayStyle
 var addButtonDisplayStyle
-var isEdit
+
+const clearForm = function() {
+  document.querySelector( '#title' ).value = ""
+  document.querySelector( '#author' ).value = ""
+  document.querySelector( '#genre' ).value = ""
+}
 
 const displayTable = function( json ) {
-  console.log( json )
 
   var col = [];
   for (var i = 0; i < json.length; i++) {
@@ -23,196 +26,234 @@ const displayTable = function( json ) {
   // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
   var tr = table.insertRow(-1)                   // TABLE ROW.
 
-      for (var i = 0; i < col.length; i++) { // change this 
-        var th = document.createElement( "th" )      
-        th.innerHTML = col[i]
-        tr.appendChild(th)
-      }
+  for (var i = 0; i < col.length; i++) { // change this 
+    var th = document.createElement( "th" )      
+    th.innerHTML = col[i]
+    tr.appendChild(th)
+  }
 
-      // ADD JSON DATA TO THE TABLE AS ROWS.
-      for (var i = 0; i < json.length; i++) {
-        tr = table.insertRow(-1)
+  // ADD JSON DATA TO THE TABLE AS ROWS.
+  for (var i = 0; i < json.length; i++) {
+    tr = table.insertRow(-1)
 
-        for (var j = 0; j < col.length; j++) {
-          var tabCell = tr.insertCell(-1)
+    for (var j = 0; j < col.length; j++) {
+      var tabCell = tr.insertCell(-1)
           tabCell.innerHTML = json[i][col[j]]
-        }
-        var x = document.createElement("INPUT");
-        x.setAttribute("type", "checkbox");
-        x.onclick = function() {
-          row = this.parentNode
-          //console.log(row)
-          var inputs = document.getElementsByTagName("input");
-          for(var i = 0; i < inputs.length; i++) {
-            if(inputs[i].type == "checkbox") {
-              inputs[i].checked = false; 
-            }  
-          }
-          this.checked= true;
-          document.getElementById( "editEntryButton" ).style.display = "block"
-        }
-        tr.append(x)
+    }
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "checkbox");
+
+    x.onclick = function() {
+      row = this.parentNode
+      var inputs = document.getElementsByTagName("input");
+      for(var i = 0; i < inputs.length; i++) {
+        if(inputs[i].type == "checkbox") {
+          inputs[i].checked = false; 
+        }  
       }
-
-    var divContainer = document.getElementById( "table" )
-    divContainer.innerHTML = ""
-    divContainer.appendChild(table)
+      this.checked= true;
+      document.getElementById( "editEntryButton" ).style.display = "block"
+    }
+    tr.append(x)
   }
 
-  const submit = function( e ) { 
-    // prevent default form action from being carried out
-    e.preventDefault()
-    document.getElementById( "formDisplay" ).style.display = "none"
-    document.getElementById( "addNewEntry" ).style.display = "block"
+  var divContainer = document.getElementById( "table" )
+  divContainer.innerHTML = ""
+  divContainer.appendChild(table)
+}
 
-    const title = document.querySelector( '#title' ),
-          author = document.querySelector( '#author' ),
-          genre = document.querySelector( '#genre' ),
-          json = { 'title':title.value,
-                   'author':author.value,
-                   'genre':genre.value,
-                 },
-          body = JSON.stringify( json )
-          console.log( json )
+const submit = function( e ) { 
+  // prevent default form action from being carried out
+  e.preventDefault()
 
-          document.querySelector( '#title' ).value = ""
-          document.querySelector( '#author' ).value = ""
-          document.querySelector( '#genre' ).value = ""
+  //close form
+  document.getElementById( "formDisplay" ).style.display = "none"
+  document.getElementById( "addNewEntry" ).style.display = "block"
 
-    fetch( '/data/add', {
-      method:'POST',
-      body, 
-      headers: {'Content-Type':'application/json'}
-    })
-    .then( function( response ) {
-      console.log( response )
-      return response.json()
-    }).then( function( json ) {
-      displayTable( json )
-    })
-    return false
-  }
+  const title = document.querySelector( '#title' ),
+        author = document.querySelector( '#author' ),
+        genre = document.querySelector( '#genre' ),
+        json = { 'title':title.value,
+                  'author':author.value,
+                  'genre':genre.value,
+                },
+        body = JSON.stringify( json )
 
-  const getData = function() {
-    fetch('/data', {
-      method: 'GET'
-    })
-    .then( function( response ) {
-      console.log( response )
-      return response.json()
-    }).then( function( json ) {
-      displayTable( json )
-    })
-    return false
-  }
+  // clear form
+  //document.querySelector( '#title' ).value = ""
+  //document.querySelector( '#author' ).value = ""
+  //document.querySelector( '#genre' ).value = ""
+  clearForm()
 
-  const submitEdit = function() {
+  fetch( '/data/add', {
+    method:'POST',
+    body, 
+    headers: {'Content-Type':'application/json'}
+  })
+  .then( function( response ) {
+    console.log( response )
+    return response.json()
+  }).then( function( json ) {
+    displayTable( json )
+  })
+  return false
+}
 
-    document.getElementById( "formDisplay" ).style.display = "none"
-    document.getElementById( "addNewEntry" ).style.display = "block"
-    document.getElementById( "editEntryButton" ).style.display = "block"
+const getData = function() {
+  fetch('/data', {
+    method: 'GET'
+  })
+  .then( function( response ) {
+    console.log( response )
+    return response.json()
+  }).then( function( json ) {
+    displayTable( json )
+  })
+  return false
+}
 
-    const entryKey = row.firstElementChild.innerHTML,
-          title = document.querySelector( '#title' ),
-          author = document.querySelector( '#author' ),
-          genre = document.querySelector( '#genre' ),
-          json = { 'title': entryKey,
-                   'newEntry': 
-                   {
+const submitEdit = function() {
+
+  // close form
+  document.getElementById( "formDisplay" ).style.display = "none"
+  document.getElementById( "addNewEntry" ).style.display = "block"
+  document.getElementById( "editEntryButton" ).style.display = "none"
+
+
+
+  const entryKey = row.firstElementChild.innerHTML,
+        title = document.querySelector( '#title' ),
+        author = document.querySelector( '#author' ),
+        genre = document.querySelector( '#genre' ),
+        json = { 'title': entryKey,
+                 'newEntry': 
+                 {
                     'title':title.value,
                     'author':author.value,
                     'genre':genre.value,
-                   }
-                 },
-          body = JSON.stringify( json )
+                  }
+                },
+        body = JSON.stringify( json )
 
-    document.querySelector( '#title' ).value = ""
-    document.querySelector( '#author' ).value = ""
-    document.querySelector( '#genre' ).value = ""      
-    console.log(body)   
 
-    fetch('/data/edit', {
-      method: 'POST',
-      body,
-      headers: {'Content-Type':'application/json'}
-    })
-    .then( function ( response ) {
-      console.log( response )
-      return response.json()
-    })
-    .then (function( json ) {
-      displayTable( json );
-    })
-    return false
-  }
+  //clear form      
+  //document.querySelector( '#title' ).value = ""
+  //document.querySelector( '#author' ).value = ""
+  //document.querySelector( '#genre' ).value = ""  
+  clearForm()     
 
-  const submitDelete = function() {
-    const entryKey = row.firstElementChild.innerHTML,
-          json = { 
-                  'title': entryKey,
-                 },
-          body = JSON.stringify( json )
+  fetch('/data/edit', {
+    method: 'POST',
+    body,
+    headers: {'Content-Type':'application/json'}
+  })
+  .then( function ( response ) {
+    console.log( response )
+    return response.json()
+  })
+  .then (function( json ) {
+    displayTable( json );
+  })
+  return false
+}
+
+const submitDelete = function() {
+  const entryKey = row.firstElementChild.innerHTML,
+        json = { 
+                'title': entryKey,
+               },
+        body = JSON.stringify( json )
     
-    document.querySelector( '#title' ).value = ""
-    document.querySelector( '#author' ).value = ""
-    document.querySelector( '#genre' ).value = ""    
+    
+      //clear form
+  document.querySelector( '#title' ).value = ""
+  document.querySelector( '#author' ).value = ""
+  document.querySelector( '#genre' ).value = ""    
+  clearForm()
 
-    fetch('/data/delete', {
-      method: 'POST',
-      body,
-      headers: {'Content-Type':'application/json'}
-    })
-    .then( function ( response ) {
-      console.log( response )
-      return response.json()
-    })
-    .then (function( json ){
-      displayTable( json );
-    })
-    return false
-  }
+  fetch('/data/delete', {
+    method: 'POST',
+    body,
+    headers: {'Content-Type':'application/json'}
+  })
+  .then( function ( response ) {
+    console.log( response )
+    return response.json()
+  })
+  .then (function( json ){
+    displayTable( json );
+  })
+  return false
+}
 
-  const editData = function() {
-    isEdit = true;
-    document.querySelector( '#title' ).value = row.children[0].innerHTML
-    document.querySelector( '#author' ).value = row.children[1].innerHTML
-    document.querySelector( '#genre' ).value = row.children[2].innerHTML 
+const editData = function() {
+  document.querySelector( '#title' ).value = row.children[0].innerHTML
+  document.querySelector( '#author' ).value = row.children[1].innerHTML
+  document.querySelector( '#genre' ).value = row.children[2].innerHTML 
 
-    document.getElementById( "deleteEntryButton" ).style.display = "block"
-    document.getElementById( "editEntryButton" ).style.display = "none"
+  // display edit form
+  document.getElementById( "deleteEntryButton" ).style.display = "block"
+  document.getElementById( "formDisplay" ).style.display = "block"
+  document.getElementById( "editEntryButton" ).style.display = "none"
+  document.getElementById( "addNewEntry" ).style.display = "none"
 
-    document.getElementById( "formDisplay" ).style.display = "block"
-    document.getElementById( "addNewEntry" ).style.display = "none"
-    const deleteButton = document.getElementById( 'deleteEntryButton' )
+  const deleteButton = document.getElementById( 'deleteEntryButton' )
     deleteButton.onclick = function() {
+
+      //close form
       document.getElementById( "formDisplay" ).style.display = "none"
       document.getElementById( "editEntryButton" ).style.display = "none"
       document.getElementById( "addNewEntry" ).style.display = "block"
+
+
       submitDelete()
     }
     const cancelButton = document.getElementById( 'cancelEditButton' )
     cancelButton.onclick = function() {
       console.log("clicked")
+
+      // close form maintain edit
       document.getElementById( "formDisplay" ).style.display = "none"
-      document.getElementById( "editEntryButton" ).style.display = "block"
       document.getElementById( "addNewEntry" ).style.display = "block"
 
-      document.querySelector( '#title' ).value = ""
-      document.querySelector( '#author' ).value = ""
-      document.querySelector( '#genre' ).value = ""     
+      document.getElementById( "editEntryButton" ).style.display = "block"
+
+      //clear form
+      //document.querySelector( '#title' ).value = ""
+      //document.querySelector( '#author' ).value = ""
+      //document.querySelector( '#genre' ).value = ""    
+      clearForm() 
     }
     const button = document.getElementById( 'button1' )
     button.onclick = submitEdit
   }
   
   const displayForm = function() {
+
+    //show form for new entry
     document.getElementById( "addNewEntry" ).style.display = "none"
     document.getElementById( "formDisplay" ).style.display = "block"
     document.getElementById( "deleteEntryButton" ).style.display = "none"
+    document.getElementById( "editEntryButton" ).style.display = "none"
+
+    //clears check boxes
+    var inputs = document.getElementsByTagName("input");
+    for(var i = 0; i < inputs.length; i++) {
+      if(inputs[i].type == "checkbox") {
+        inputs[i].checked = false; 
+      }  
+    }
+
     const cancelButton = document.getElementById( 'cancelEditButton' )
     cancelButton.onclick = function() {
+
+      
+      // hide form 
       document.getElementById( "formDisplay" ).style.display = "none"
-      document.getElementById( "addNewEntry" ).style.display = "block"    
+      document.getElementById( "addNewEntry" ).style.display = "block"  
+
+
+
     }
     const button = document.getElementById( 'button1' ) 
     button.onclick = submit
@@ -223,15 +264,13 @@ const displayTable = function( json ) {
     newEntryButton.onclick = displayForm
 
     const editEntryButton = document.getElementById( 'editEntryButton' )
+
+    //first condition
     document.getElementById( "editEntryButton" ).style.display = "none"
     document.getElementById( "formDisplay" ).style.display = "none"
 
     editEntryButton.onclick = editData
     table = document.createElement( "table" )
-    editButton = document.getElementById( "editEntryButton" )
-
-    formDisplayStyle = document.getElementById( "formDisplay" ).style.display
-    addButtonDisplayStyle = document.getElementById( "addNewEntry" ).style.display
 
     getData()
     
